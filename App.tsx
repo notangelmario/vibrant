d29130005906978dcac1c192bb58bf13478f3e95
notangelmario <savin@angelmario.eu>
2021-03-04 15:08:24 +0200

    Added basic react native navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,68 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { TransitionSpecs, CardStyleInterpolators ,createStackNavigator } from '@react-navigation/stack'
+import { Ionicons } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, StatusBar, Text, View } from 'react-native';
+
+const Tab = createBottomTabNavigator()
+const Stack = createStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Vibrant</Text>
-      <StatusBar barStyle="light-content" translucent backgroundColor='transparent'/>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: any;
+
+            switch (route.name) {
+              case 'Home':
+                iconName = focused ? 'musical-notes' : 'musical-notes-outline';
+                break;
+              case 'Settings':
+                iconName = focused ? 'settings' : 'settings-outline';
+                break;
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size + (focused ? 5 : 0)} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{title: 'Overview'}}/>
+        <Tab.Screen name="Settings" component={SettingsScreen}/>
+      </Tab.Navigator>
+      <StatusBar barStyle='dark-content' translucent backgroundColor='transparent'/>
+    </NavigationContainer>
+  );
+}
+
+
+function HomeScreen(props: any) {
+  const { navigation } = props
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title='Go to details'
+        onPress={() => props.navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#212121',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 20
-  }
-});
+function SettingsScreen(props: any) {
+  const { navigation } = props
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
