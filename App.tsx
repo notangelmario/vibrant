8ba1c1429c5bb532b676f108d1287f594e40aa92
notangelmario <savin@angelmario.eu>
2021-03-04 11:19:46 +0200

    Adjusted settings

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Welcome to Vibrant</Text>
+      <StatusBar barStyle="light-content" translucent backgroundColor='transparent'/>
     </View>
   );
 }
@@ -14,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212121',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
