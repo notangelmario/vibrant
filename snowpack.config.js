46dac833122ea4c7a50dab494138afdd473c8a88
notangelmario <savin@angelmario.eu>
2021-03-06 19:16:42 +0200

    Started pwa version

---
+++ snowpack.config.js
@@ -0,0 +1,46 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+const path = require('path')
+const WorkboxPlugin= require('workbox-webpack-plugin')
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+	mount: {
+		public: '/',
+		src: '/'
+	},
+	devOptions: {
+		port: 3000,
+		open: 'none',
+	},
+	plugins: [
+		[
+			'@snowpack/plugin-webpack',
+			{
+				sourceMap: false,
+				htmlMinifierOptions: {
+					collapseWhitespace: true,
+					removeComments: true,
+					removeEmptyAttributes: true,
+					removeRedundantAttributes: true,
+					removeScriptTypeAttributes: true,
+					removeStyleLinkTypeAttributes: true,
+				},
+				extendConfig: (config) => {
+					config.output = {
+						filename: '[id].mauv.js',
+						path: path.resolve(__dirname, 'build/'),
+					}
+					config.plugins.push(
+						new WorkboxPlugin.GenerateSW({
+							clientsClaim: true,
+							skipWaiting: true,
+						  }),
+					)
+					return config
+				}
+			},
+		],
+	],	
+};
