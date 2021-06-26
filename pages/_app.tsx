d60d2396de87fdda2f76a432c8d772515a46b2c5
notangelmario <savin@angelmario.eu>
2021-06-26 17:27:08 +0000

    Added a basic homescreen Added material Removed next-offline Added better native app feel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+import { Shadows } from '@material-ui/core/styles/shadows';
+import { CssBaseline } from '@material-ui/core';
+
+
+const theme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: { main: '#61c9a8'},
+		secondary: { main: '#48acf0' }
+	},
+	shape: {
+		borderRadius: 20
+	},
+	shadows: Array(25).fill("none") as Shadows,
+	components: {
+		MuiContainer: {
+			defaultProps: {
+				maxWidth: 'md'
+			}
+		},
+		MuiCard: {
+			defaultProps: {
+				variant: 'outlined'
+			}
+		}
+	}
+});
 
 function Vibrant({ Component, pageProps }: AppProps) {
 	return (
@@ -15,8 +43,14 @@ function Vibrant({ Component, pageProps }: AppProps) {
 				<meta name="application-name" content="Vibrant"/>
 				<meta name="msapplication-TileColor" content="#48acf0"/>
 				<meta name="theme-color" content="#48acf0"></meta>
+				<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"></meta>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         	</Head>
-        	<Component {...pageProps} />
+			<ThemeProvider theme={theme}>
+				<CssBaseline/>
+        		<Component {...pageProps} />
+			</ThemeProvider>
     	</>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,43 @@
-import Head from 'next/head'
+import { Avatar, Container, Grid, Typography, Box, CardContent, Card, Icon } from '@material-ui/core'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-				</a>
-			</footer>
-		</div>
+		<Container>
+			<Grid
+				sx={{padding: theme => `${theme.spacing(2)} 0`}}
+				container
+				direction='column'
+				spacing={2}
+			>
+				<Grid item>
+					<Typography variant='h2' sx={{color: theme=>theme.palette.primary.main}}>
+						Welcome to Vibrant!
+					</Typography>
+				</Grid>
+				<Grid item>
+					<Typography gutterBottom>
+						Vibrant is a music app bringing you the features that you miss from
+						other music players.
+					</Typography>
+				</Grid>				
+				<Grid item>
+					<Typography>
+						Vibrant at the moment is under construction. We are working 
+						hard to make Vibrant available as soon as possible.
+					</Typography>
+				</Grid>
+				<Grid item>
+					<Card sx={{backgroundColor: theme => theme.palette.info.main}}>
+						<CardContent>
+							<Icon fontSize='medium'>info</Icon>{" "}
+							While the app is under development, you can try it anyways.
+							Remember that the Vibrant is under heavy development and is not
+							stable at all. Vibrant is not really reliable at the current state.
+						</CardContent>
+					</Card>
+				</Grid>
+			</Grid>
+		</Container>
 	)
 }
