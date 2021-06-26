aa691ef9d01e2a7706b0b553dc1fb7993619c6fe
notangelmario <savin@angelmario.eu>
2021-06-26 08:35:21 +0000

    Configure repo
    Configure eslint
    Added assets
    Configure site manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function Vibrant({ Component, pageProps }: AppProps) {
+	return 
+	(
+    	<>
+        	<Head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+				<link rel="manifest" href="/site.webmanifest"/>
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#48acf0"/>
+				<meta name="apple-mobile-web-app-title" content="Vibrant"/>
+				<meta name="application-name" content="Vibrant"/>
+				<meta name="msapplication-TileColor" content="#48acf0"/>
+				<meta name="theme-color" content="#48acf0"></meta>
+        	</Head>
+        	<Component {...pageProps} />
+    	</>
+	)
 }
-export default MyApp
